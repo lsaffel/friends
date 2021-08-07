cf600e029bee46a47387c24a7c0110e793228fb6
Lisa <llvvss@icloud.com>
2021-08-07 10:28:11 -0600

    preparing to merge

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_07_17_014040) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
